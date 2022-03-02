e232754bb722ca15e8e10b80c299fad126860c1e
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-02 15:15:26 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace alistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("dizi sayisini giriniz: ");
+             int ar_nu = int.Parse(Console.ReadLine());
+             string[] dizi = new string[ar_nu];
+             for (int i=0; i<ar_nu; i++)
+             {
+                 Console.Write("{0}. kelimayi giriniz: ",i+1);
+                 dizi[i] = Console.ReadLine();
+             }
+             Array.Sort(dizi);
+             foreach (var kelima in dizi)
+             {
+             Console.WriteLine(kelima);
+             }
+        }
+    }
+    
+}
